0a4e1731136a613ce8db0eb2cb1137f2d28e766b
samvaio <samdivaio@icloud.com>
2017-02-16 16:51:32 +0530

    more

--- src/NadekoBot/Modules/Music/Classes/MusicControls.cs
+++ src/NadekoBot/Modules/Music/Classes/MusicControls.cs
@@ -191,7 +191,7 @@ namespace NadekoBot.Modules.Music.Classes
                         SongCancelSource = new CancellationTokenSource();
                         cancelToken = SongCancelSource.Token;
                         CurrentSong = null;
-                        await Task.Delay(100).ConfigureAwait(false); //lowered from 0.3s to 0.1s
+                        await Task.Delay(500).ConfigureAwait(false); // from 0.3s to 0.5s
                     }
                 }
             }));
